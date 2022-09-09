030b57a6ec937757f97a1dd76796a195f39cbe8c
Esubalew Desktop <esubalew.a2009@gmail.com>
2022-09-09 10:00:28 +0300

    initial commit

---
+++ android/app/src/main/kotlin/com/herma/millions_recipe/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.herma.millions_recipe
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/explore.dart
@@ -0,0 +1,316 @@
+import 'package:flutter/material.dart';
+import 'package:millions_recipe/constants.dart';
+import 'package:millions_recipe/data.dart';
+import 'package:millions_recipe/detail.dart';
+import 'package:millions_recipe/shared.dart';
+
+class Explore extends StatefulWidget {
+  @override
+  _ExploreState createState() => _ExploreState();
+}
+
+class _ExploreState extends State<Explore> {
+
+  List<bool> optionSelected = [true, false, false];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.grey[50],
+      appBar: AppBar(
+        backgroundColor: Colors.transparent,
+        brightness: Brightness.light,
+        elevation: 0,
+        leading: Icon( // drawer
+          Icons.sort,
+          color: Colors.black,
+        ),
+        actions: [
+          Padding(
+            padding: EdgeInsets.only(right: 16),
+            child: Icon(
+              Icons.search,
+              color: Colors.black,
+            ),
+          ),
+        ],
+      ),
+      body: SingleChildScrollView(
+        physics: BouncingScrollPhysics(),
+        child: Column(
+          children: [
+
+            Padding(
+              padding: EdgeInsets.symmetric(horizontal: 16),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  
+                  buildTextTitleVariation1('Springy Salads'),
+
+                  buildTextSubTitleVariation1('Healthy and nutritious food recipes'),
+
+                  SizedBox(
+                    height: 32,
+                  ),
+                  Row( // should be changed to list view to support horizontal scroll
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      
+                      option('Vegetable', 'assets/icons/salad.png', 0),
+                      SizedBox(
+                        width: 8,
+                      ),
+                      option('Rice', 'assets/icons/rice.png', 1),
+                      SizedBox(
+                        width: 8,
+                      ),
+                      option('Fruit', 'assets/icons/fruit.png', 2),
+
+                      SizedBox(
+                        width: 8,
+                      ),
+                      option('Fruit', 'assets/icons/fruit.png', 2),
+
+                    ],
+                  ),
+
+                ],
+              ),
+            ),
+
+            SizedBox(
+              height: 24,
+            ),
+
+            Container(
+              height: 350,
+              child: ListView(
+                physics: BouncingScrollPhysics(),
+                scrollDirection: Axis.horizontal,
+                children: buildRecipes(),
+              ),
+            ),
+
+            SizedBox(
+              height: 16,
+            ),
+
+            Padding(
+              padding: EdgeInsets.symmetric(horizontal: 16),
+              child: Row(
+                children: [
+
+                  buildTextTitleVariation2('Popular', false),
+
+                  SizedBox(
+                    width: 8,
+                  ),
+
+                  buildTextTitleVariation2('Food', true),
+
+                ],
+              ),
+            ),
+
+            Container(
+              height: 190,
+              child: PageView(
+                physics: BouncingScrollPhysics(),
+                children: buildPopulars(),
+              ),
+            ),
+
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget option(String text, String image, int index){
+    return GestureDetector(
+      onTap: () {
+        setState(() {
+          optionSelected[index] = !optionSelected[index];
+        });
+      },
+      child: Container(
+        height: 40,
+        decoration: BoxDecoration(
+          color: optionSelected[index] ? kPrimaryColor : Colors.white,
+          borderRadius: BorderRadius.all(
+            Radius.circular(20),
+          ),
+          boxShadow: [kBoxShadow],
+        ),
+        padding: EdgeInsets.symmetric(horizontal: 12),
+        child: Row(
+          children: [
+
+            SizedBox(
+              height: 32,
+              width: 32,
+              child: Image.asset(
+                image,
+                color: optionSelected[index] ? Colors.white : Colors.black,
+              ),
+            ),
+
+            SizedBox(
+              width: 8,
+            ),
+
+            Text(
+              text,
+              style: TextStyle(
+                color: optionSelected[index] ? Colors.white : Colors.black,
+                fontSize: 14,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  List<Widget> buildRecipes(){
+    List<Widget> list = [];
+    for (var i = 0; i < getRecipes().length; i++) {
+      list.add(buildRecipe(getRecipes()[i], i));
+    }
+    return list;
+  }
+
+  Widget buildRecipe(Recipe recipe, int index){
+    return GestureDetector(
+      onTap: () {
+        Navigator.push(
+          context,
+          MaterialPageRoute(builder: (context) => Detail(recipe: recipe)),
+        );
+      },
+      child: Container(
+        decoration: BoxDecoration(
+          color: Colors.white,
+          borderRadius: BorderRadius.all(
+            Radius.circular(20),
+          ),
+          boxShadow: [kBoxShadow],
+        ),
+        margin: EdgeInsets.only(right: 16, left: index == 0 ? 16 : 0, bottom: 16, top: 8),
+        padding: EdgeInsets.all(16),
+        width: 220,
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: <Widget>[
+
+            Expanded(
+              child: Hero(
+                tag: recipe.image,
+                child: Container(
+                  decoration: BoxDecoration(
+                    image: DecorationImage(
+                      image: AssetImage(recipe.image),
+                      fit: BoxFit.contain,
+                    ),
+                  ),
+                ),
+              ),
+            ),
+
+            SizedBox(
+              height: 8,
+            ),
+
+            buildRecipeTitle(recipe.title),
+
+            buildTextSubTitleVariation2(recipe.description),
+
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+
+                buildCalories(recipe.calories.toString() + " Kcal"),
+
+                Icon(
+                  Icons.favorite_border,
+                )
+
+              ],
+            ),
+
+          ],
+        ),
+      ),
+    );
+  }
+
+  List<Widget> buildPopulars(){
+    List<Widget> list = [];
+    for (var i = 0; i < getRecipes().length; i++) {
+      list.add(buildPopular(getRecipes()[i]));
+    }
+    return list;
+  }
+
+  Widget buildPopular(Recipe recipe){
+    return Container(
+      margin: EdgeInsets.all(16),
+      decoration: BoxDecoration(
+        color: Colors.white,
+        borderRadius: BorderRadius.all(
+          Radius.circular(20),
+        ),
+        boxShadow: [kBoxShadow],
+      ),
+      child: Row(
+        children: [
+
+          Container(
+            height: 160,
+            width: 160,
+            decoration: BoxDecoration(
+              image: DecorationImage(
+                image: AssetImage(recipe.image),
+                fit: BoxFit.fitHeight,
+              ),
+            ),
+          ),
+
+          Expanded(
+            child: Padding(
+              padding: EdgeInsets.symmetric(horizontal: 16),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+
+                  buildRecipeTitle(recipe.title),
+
+                  buildRecipeSubTitle(recipe.description),
+
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+
+                      buildCalories(recipe.calories.toString() + " Kcal"),
+
+                      Icon(
+                        Icons.favorite_border,
+                      )
+
+                    ],
+                  ),
+
+                ],
+              ),
+            ),
+          ),
+
+        ],
+      ),
+    );
+  }
+
+}
