77d248775446d4cd08328a44ecf41c6d067deebb
wendi-code <wendimusit@gmail.com>
2023-05-08 14:38:34 +0300

    add time to recipe_model

--- lib/models/recipe_model.dart
+++ lib/models/recipe_model.dart
@@ -10,12 +10,12 @@ class Recipe with ChangeNotifier {
   final String description;
   final String? rating;
   final String numOfRatings;
+  final String time;
   final String category;
   final List<ImageModel> images;
   final List<Ingredient> ingredients;
   final List<Nutrition> nutritions;
   final List<Step> steps;
-
 
   Recipe(
       this.id,
@@ -23,6 +23,7 @@ class Recipe with ChangeNotifier {
       this.description,
       this.rating,
       this.numOfRatings,
+      this.time,
       this.category,
       this.images,
       this.ingredients,
@@ -33,13 +34,28 @@ class Recipe with ChangeNotifier {
       : id = json['id'] as String,
         name = json['recipeName'] as String,
         description = json['recipeDescription'] as String,
-        rating = json['recipeRating'] == null ? "0" : json['recipeRating'] as String,
-        numOfRatings = json['numOfRatings'] == null ? "0" : json['numOfRatings'] as String,
+        rating =
+            json['recipeRating'] == null ? "0" : json['recipeRating'] as String,
+        numOfRatings =
+            json['numOfRatings'] == null ? "0" : json['numOfRatings'] as String,
+        time = json['totalTime'] == null
+            ? "0"
+            : json['totalTime'].replaceAll(RegExp('[^0-9]'), '') as String,
         category = json['recipeMainCategory'] as String,
-        images = (json['images'] as List<dynamic>).map((image) => ImageModel.fromJson(image as Map<String, dynamic>)).toList(),
-        ingredients = (json['ingredients'] as List<dynamic>).map((ingredient) => Ingredient.fromJson(ingredient as Map<String, dynamic>)).toList(),
-        nutritions = (json['nutritions'] as List<dynamic>).map((nutrition) => Nutrition.fromJson(nutrition as Map<String, dynamic>)).toList(),
-        steps = (json['directions'] as List<dynamic>).map((step) => Step.fromJson(step as Map<String, dynamic>)).toList();
+        images = (json['images'] as List<dynamic>)
+            .map((image) => ImageModel.fromJson(image as Map<String, dynamic>))
+            .toList(),
+        ingredients = (json['ingredients'] as List<dynamic>)
+            .map((ingredient) =>
+                Ingredient.fromJson(ingredient as Map<String, dynamic>))
+            .toList(),
+        nutritions = (json['nutritions'] as List<dynamic>)
+            .map((nutrition) =>
+                Nutrition.fromJson(nutrition as Map<String, dynamic>))
+            .toList(),
+        steps = (json['directions'] as List<dynamic>)
+            .map((step) => Step.fromJson(step as Map<String, dynamic>))
+            .toList();
 
   Map<String, dynamic> toJson() => {
         'id': id,
@@ -47,11 +63,11 @@ class Recipe with ChangeNotifier {
         'recipeDescription': description,
         'recipeRating': rating,
         'numOfRatings': numOfRatings,
+        'totlatime': 'PT${time}M',
         'recipeMainCategory': category,
         'images': images,
         'ingredients': ingredients,
         'nutritions': nutritions,
         'directions': steps,
-        
       };
 }
