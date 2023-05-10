dd2c3eb9151f3f272e6e03b941f102c5e6c9766d
z-qine <zqine@localhost.localdomain>
2023-05-10 15:56:35 +0300

    full implementation of auth

--- lib/api_service/api_provider.dart
+++ lib/api_service/api_provider.dart
@@ -8,77 +8,64 @@ import '../models/auth_model.dart';
 import 'constants.dart';
 
 class ApiProvider {
-
-  Future<String> registerUser(String email, String firstname, String lastname, String password, String registerWith) async {
-    String res = "Some error is occured";
+  Future<String> registerUser(String email, String firstname, String lastname,
+      String password, String registerWith) async {
+    String res = "Some error occurred";
+    Dio dio = Dio();
 
     try {
       String passwordParam = "password";
       String firstNameParam = "first_name";
       String lastNameParam = "last_name";
+
       if (registerWith == "google") {
         passwordParam = "google_user_id";
         firstNameParam = "given_name";
         lastNameParam = "family_name";
       }
-      var dio = Dio();
-      Map<String, dynamic> requestBody = {
-        "registed_with": registerWith,
-        "email": email,
-        firstNameParam: firstname,
-        lastNameParam: lastname,
-        passwordParam: password
-      };
-      Response response = await dio.post(
+
+      var response = await dio.post(
         AppUrl.userregisterUrl,
-        data: requestBody,
+        options: Options(headers: {
+          'Content-Type': 'application/json; charset=UTF-8',
+        }),
+        data: {
+          "registed_with": registerWith,
+          "email": email,
+          firstNameParam: firstname,
+          lastNameParam: lastname,
+          passwordParam: password,
+        },
       );
+
       if (response.statusCode == 200) {
         res = "success";
 
         if (registerWith == "google") {
-          var userInfo = jsonDecode(response.data.toString());
-          String? image = "https://www.gravatar.com/avatar/205e460b479e2e5b48aec07710c08d50";
+          var userInfo = response.data;
+          String? image =
+              "https://www.gravatar.com/avatar/205e460b479e2e5b48aec07710c08d50";
           if (userInfo['image'] != null) {
             image = userInfo['image'];
           }
           UserPreferences.setuser(image!, userInfo['username']!, userInfo['first_name'], userInfo['last_name'], email.toString());
         }
-      } else {}
-    } catch (e) {
-      if (e is DioError) {
-        Response response = e.response!;
-        res = response.data['message'];
-      }
-    }
-
-    return res;
-  }
-
-  Future<String> verification(String email, int otp) async {
-    String res = "Some error is occured";
-    try {
-      var dio = Dio();
-      Map<String, dynamic> requestBody = {"email": email, "code": otp};
-      Response response = await dio.post(
-        AppUrl.activateaccount,
-        data: requestBody,
-      );
-      if (response.statusCode == 200) {
-        res = "success";
       } else {
-        var temp = jsonDecode(response.data.toString());
+        var temp = response.data;
         String message = temp['message'];
         res = message;
       }
-    } catch (_) {
+    } on DioError catch (e) {
+      Response response = e.response!;
+      res = response.data['message'];
+    } on Exception {
       res = "Some error is occured";
     }
-
     return res;
   }
 
-  Future<String> loginUser(String email, String password, String loginWith) async {
+  Future<String> loginUser(
+      String email, String password, String loginWith) async {
     String res = "Some error is occured";
     try {
       var dio = Dio();
@@ -93,29 +80,144 @@ class ApiProvider {
 
       if (response.statusCode == 200) {
         res = "success";
-        final responseBody = response.data;
-        final AuthModel authModel = authModelFromJson(responseBody);
-        final SharedPreferences prefs = await SharedPreferences.getInstance();
-
-        prefs.setString("token", authModel.token);
-
-        UserPreferences.setuser(
-          authModel.userImage.toString(),
-          authModel.username.toString(),
-          authModel.userFirstName.toString(),
-          authModel.userLastName.toString(),
-          authModel.userEmail.toString(),
-        );
-      }
-    } catch (e) {
-      if (e is DioError) {
-        Response response = e.response!;
-        res = response.data['message'];
       } else {
-        res = "Some error is occured";
+        String? message = response.statusMessage;
+        res = message!;
       }
+    } on DioError catch (e) {
+      Response response = e.response!;
+      res = response.data['message'];
+    } on Exception {
+      res = "Some error is occured";
     }
     return res;
   }
 
+  Future<String> verification(String email, int otp) async {
+    String res = "Some error occurred";
+    Dio dio = Dio();
+    try {
+      Response response = await dio.post(
+        AppUrl.activateaccount,
+        options: Options(
+          headers: {'Content-Type': 'application/json; charset=UTF-8'},
+        ),
+        data: jsonEncode(<String, dynamic>{"email": email, "code": otp}),
+      );
+      if (response.statusCode == 200) {
+        res = "success";
+      } else {
+        var temp = jsonDecode(response.data.toString());
+        String message = temp['message'];
+        res = message;
+      }
+    } on DioError catch (e) {
+      Response response = e.response!;
+      res = response.data['message'];
+    } on Exception {
+      res = "Some error is occured";
+    }
+
+    return res;
+  }
+
+  Future<String> resendActivation(String email) async {
+    String res = "Some error occurred";
+    Dio dio = Dio();
+    try {
+      Response response = await dio.post(
+        AppUrl.resendActivation,
+        options: Options(
+          headers: {'Content-Type': 'application/json; charset=UTF-8'},
+        ),
+        data: jsonEncode(<String, dynamic>{"email": email}),
+      );
+      if (response.statusCode == 200) {
+        res = "success";
+      } else {
+        var temp = jsonDecode(response.data.toString());
+        String message = temp['message'];
+        res = message;
+      }
+    } on DioError catch (e) {
+      if (e.response != null) {
+        var temp = jsonDecode(e.response!.data.toString());
+        String message = temp['message'];
+        res = message;
+      } else {
+        res = e.toString();
+        // res = "Some error occurred";
+      }
+    } catch (e) {
+      res = e.toString();
+    }
+
+    return res;
+  }
+
+  Future<String> sendInstruction(String email) async {
+    String res = "Some error occurred";
+    Dio dio = Dio();
+
+    try {
+      var response = await dio.post(
+        AppUrl.sendInstraction,
+        data: {"user_login": email},
+        options: Options(
+          contentType: 'application/json; charset=UTF-8',
+        ),
+      );
+
+      if (response.statusCode == 200) {
+        res = "success";
+      } else {
+        var temp = response.data;
+        String message = temp['message'];
+        res = message;
+      }
+    } on DioError catch (e) {
+      Response response = e.response!;
+      res = response.data['message'];
+    } on Exception {
+      res = "Some error is occured";
+    }
+
+    return res;
+  }
+
+  Future<String> setNewPassword(String email, String newPass, int code) async {
+    String res = "Some error occurred";
+    Dio dio = Dio();
+
+    try {
+      Response response = await dio.post(
+        AppUrl.setnewpassword,
+        options: Options(
+          headers: {
+            'Content-Type': 'application/json; charset=UTF-8',
+          },
+        ),
+        data: {
+          "email": email,
+          "password": newPass,
+          "code": code,
+        },
+      );
+
+      if (response.statusCode == 200) {
+        res = "success";
+      } else {
+        var temp = response.data;
+        String message = temp['message'];
+        res = message;
+      }
+    } on DioError catch (e) {
+      Response response = e.response!;
+      res = response.data['message'];
+    } on Exception {
+      res = "Some error is occured";
+    }
+
+    return res;
+  }
 }
--- lib/auth/otp_verification.dart
+++ lib/auth/otp_verification.dart
@@ -1,13 +1,21 @@
+import 'package:another_flushbar/flushbar.dart';
 import 'package:flutter/material.dart';
 
+import '../api_service/api_provider.dart';
+import 'login.dart';
+
 class Otp extends StatefulWidget {
-  const Otp({Key? key}) : super(key: key);
+  final String email;
+  const Otp({super.key, required this.email});
 
   @override
+  // ignore: library_private_types_in_public_api
   _OtpState createState() => _OtpState();
 }
 
 class _OtpState extends State<Otp> {
+  // ignore: prefer_final_fields
+  List<String> _otpValues = ['', '', '', '', ''];
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -70,11 +78,11 @@ class _OtpState extends State<Otp> {
                     Row(
                       mainAxisAlignment: MainAxisAlignment.center,
                       children: [
-                        _textFieldOTP(first: true, last: false),
-                        _textFieldOTP(first: false, last: false),
-                        _textFieldOTP(first: false, last: false),
-                        _textFieldOTP(first: false, last: false),
-                        _textFieldOTP(first: false, last: true),
+                        _textFieldOTP(first: true, last: false, index: 0),
+                        _textFieldOTP(first: false, last: false, index: 1),
+                        _textFieldOTP(first: false, last: false, index: 2),
+                        _textFieldOTP(first: false, last: false, index: 3),
+                        _textFieldOTP(first: false, last: true, index: 4),
                       ],
                     ),
                     const SizedBox(
@@ -83,7 +91,7 @@ class _OtpState extends State<Otp> {
                     GestureDetector(
                       onTap: () {
                         // todo:
-                        //
+                        verify();
                       },
                       child: Container(
                         margin: const EdgeInsets.only(top: 16),
@@ -128,14 +136,17 @@ class _OtpState extends State<Otp> {
               const SizedBox(
                 height: 20,
               ),
-              const Text(
-                "Resend New Code",
-                style: TextStyle(
-                  fontSize: 18,
-                  fontWeight: FontWeight.bold,
-                  color: Color(0xff53E88B),
+              GestureDetector(
+                onTap: () => resendActivation(),
+                child: const Text(
+                  "Resend New Code",
+                  style: TextStyle(
+                    fontSize: 18,
+                    fontWeight: FontWeight.bold,
+                    color: Color(0xff53E88B),
+                  ),
+                  textAlign: TextAlign.center,
                 ),
-                textAlign: TextAlign.center,
               ),
             ],
           ),
@@ -144,7 +155,78 @@ class _OtpState extends State<Otp> {
     );
   }
 
-  Widget _textFieldOTP({bool? first, bool? last}) {
+  Future verify() async {
+    if (_otpValues.length >= 4) {
+      int code = int.parse(_otpValues.join(''));
+      showDialog(
+          context: context,
+          barrierDismissible: false,
+          builder: (context) => const Center(
+                child: CircularProgressIndicator(color: Colors.blue),
+              ));
+
+      String res = await ApiProvider().verification(widget.email, code);
+
+      if (res == "success") {
+        // ignore: use_build_context_synchronously
+        Navigator.of(context).push(MaterialPageRoute(
+          builder: (context) => const LogIn(),
+        ));
+      } else {
+        // ignore: use_build_context_synchronously
+        Flushbar(
+          flushbarPosition: FlushbarPosition.BOTTOM,
+          margin: const EdgeInsets.fromLTRB(10, 20, 10, 5),
+          titleSize: 20,
+          messageSize: 17,
+          backgroundColor: Colors.blue,
+          borderRadius: BorderRadius.circular(8),
+          message: res,
+          duration: const Duration(seconds: 5),
+        ).show(context);
+      }
+    } else {
+      return null;
+    }
+  }
+
+  Future resendActivation() async {
+    showDialog(
+        context: context,
+        barrierDismissible: false,
+        builder: (context) => const Center(
+              child: CircularProgressIndicator(color: Colors.blue),
+            ));
+    String res = await ApiProvider().resendActivation(widget.email);
+
+    if (res == "success") {
+      // ignore: use_build_context_synchronously
+      Flushbar(
+        flushbarPosition: FlushbarPosition.BOTTOM,
+        margin: const EdgeInsets.fromLTRB(10, 20, 10, 5),
+        titleSize: 20,
+        messageSize: 17,
+        backgroundColor: Colors.blue,
+        borderRadius: BorderRadius.circular(8),
+        message: 'sent, check your email !!',
+        duration: const Duration(seconds: 3),
+      ).show(context);
+    } else {
+      // ignore: use_build_context_synchronously
+      Flushbar(
+        flushbarPosition: FlushbarPosition.BOTTOM,
+        margin: const EdgeInsets.fromLTRB(10, 20, 10, 5),
+        titleSize: 20,
+        messageSize: 17,
+        backgroundColor: Colors.blue,
+        borderRadius: BorderRadius.circular(8),
+        message: res,
+        duration: const Duration(seconds: 5),
+      ).show(context);
+    }
+  }
+
+  Widget _textFieldOTP({bool? first, bool? last, required int index}) {
     return Expanded(
       child: Container(
         padding: const EdgeInsets.only(left: 2, right: 3),
@@ -154,6 +236,10 @@ class _OtpState extends State<Otp> {
           child: TextField(
             autofocus: true,
             onChanged: (value) {
+              setState(() {
+                _otpValues[index] = value;
+              });
+
               if (value.length == 1 && last == false) {
                 FocusScope.of(context).nextFocus();
               }
