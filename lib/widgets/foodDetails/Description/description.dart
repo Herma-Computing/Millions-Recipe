76ff7196bc11a32556ebf64144f81ad1ba2d45e1
z-qine <zqine@localhost.localdomain>
2023-05-19 14:19:38 +0300

    Added description of meal to the detail page

---
+++ lib/widgets/foodDetails/Description/description.dart
@@ -0,0 +1,115 @@
+import 'package:flutter/material.dart';
+
+import '../../../models/recipe_model.dart';
+
+class Descriptions extends StatefulWidget {
+  final Recipe meal;
+  const Descriptions({required this.meal, super.key});
+
+  @override
+  State<Descriptions> createState() => _DescriptionsState();
+}
+
+class _DescriptionsState extends State<Descriptions> {
+  @override
+  Widget build(BuildContext context) {
+    return SingleChildScrollView(
+      child: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 12),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            const SizedBox(
+              height: 22,
+            ),
+            const Text(
+              "Description",
+              style: TextStyle(
+                  fontSize: 16,
+                  fontWeight: FontWeight.w700,
+                  color: Colors.black),
+            ),
+            const SizedBox(
+              height: 8,
+            ),
+            Text(
+              widget.meal.description,
+              style: const TextStyle(
+                  fontSize: 14,
+                  fontWeight: FontWeight.w400,
+                  color: Colors.black54),
+            ),
+            const SizedBox(
+              height: 13,
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                const Text(
+                  "Preparation Time:",
+                  style: TextStyle(fontWeight: FontWeight.w700, fontSize: 12),
+                ),
+                Text(
+                  "${widget.meal.time}min",
+                  style: const TextStyle(
+                      fontWeight: FontWeight.w700, fontSize: 12),
+                )
+              ],
+            ),
+            const SizedBox(
+              height: 13,
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                const Text(
+                  "Cooking Time:",
+                  style: TextStyle(fontWeight: FontWeight.w700, fontSize: 12),
+                ),
+                Text(
+                  "${widget.meal.time}min",
+                  style: const TextStyle(
+                      fontWeight: FontWeight.w700, fontSize: 12),
+                )
+              ],
+            ),
+            const SizedBox(
+              height: 13,
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                const Text(
+                  "Additional Time:",
+                  style: TextStyle(fontWeight: FontWeight.w700, fontSize: 12),
+                ),
+                Text(
+                  "${widget.meal.time}min",
+                  style: const TextStyle(
+                      fontWeight: FontWeight.w700, fontSize: 12),
+                )
+              ],
+            ),
+            const SizedBox(
+              height: 13,
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                const Text(
+                  "Refrigeration Time:",
+                  style: TextStyle(fontWeight: FontWeight.w700, fontSize: 12),
+                ),
+                Text(
+                  "${widget.meal.time}min",
+                  style: const TextStyle(
+                      fontWeight: FontWeight.w700, fontSize: 12),
+                )
+              ],
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
