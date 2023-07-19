d7f1b7be7ea4b32433ef7e142a908ceaf06ae692
Esubalew <esubalew.a2009@gmail.com>
2023-07-19 13:35:14 +0300

    const layout bug fix

--- lib/screens/add_recipe.dart
+++ lib/screens/add_recipe.dart
@@ -381,8 +381,8 @@ class _AddRecipeState extends State<AddRecipe> {
                         decoration: BoxDecoration(
                           border: Border.all(color: Colors.black),
                         ),
-                        child: const Row(
-                          children: [
+                        child: Row(
+                          children: const [
                             Icon(Icons.add),
                             Text(
                               "ADD STEP",
@@ -528,8 +528,8 @@ class _AddRecipeState extends State<AddRecipe> {
                       ingredients.add(Ingredient(item: '', quantity: ''));
                     });
                   },
-                  child: const Row(
-                    children: [
+                  child:  Row(
+                    children: const [
                       Icon(Icons.add),
                       Text(
                         "Add new ingredient",
