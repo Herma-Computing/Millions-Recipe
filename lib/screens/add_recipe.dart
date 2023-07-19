34d2810db09403fab482944c79f786594d2134bd
z-qine <zqine@localhost.localdomain>
2023-07-19 13:14:42 +0300

    Implementing and improving addRecipes page

--- lib/screens/add_recipe.dart
+++ lib/screens/add_recipe.dart
@@ -5,17 +5,19 @@ import 'package:image_picker/image_picker.dart';
 import 'package:flutter/material.dart';
 
 class AddRecipe extends StatefulWidget {
-  const AddRecipe({
-    super.key,
-  });
+  const AddRecipe({Key? key}) : super(key: key);
+
   @override
   State<AddRecipe> createState() => _AddRecipeState();
 }
 
 class _AddRecipeState extends State<AddRecipe> {
+  bool showHidden = false;
   List<Ingredient> ingredients = [];
   List selectedImages = [];
   File? selectedImage;
+  List<String> cookingSteps = [];
+  List<File?> stepImages = [];
 
   void selectFile({required ImageSource source}) async {
     final file = await ImagePicker().pickImage(source: source);
@@ -24,6 +26,32 @@ class _AddRecipeState extends State<AddRecipe> {
       setState(() {
         selectedImage = File(file!.path);
         selectedImages.add(selectedImage);
+      });
+    }
+  }
+
+  void addCookingStep() {
+    setState(() {
+      cookingSteps.add('');
+      stepImages.add(null);
+    });
+  }
+
+  void removeLastCookingStep() {
+    if (cookingSteps.isNotEmpty) {
+      setState(() {
+        cookingSteps.removeLast();
+        stepImages.removeLast();
+      });
+    }
+  }
+
+  void selectStepImage(int index) async {
+    final file = await ImagePicker().pickImage(source: ImageSource.gallery);
+
+    if (file?.path != null) {
+      setState(() {
+        stepImages[index] = File(file!.path);
       });
     }
   }
@@ -239,6 +267,134 @@ class _AddRecipeState extends State<AddRecipe> {
                   ],
                 ),
               ),
+              const SizedBox(height: 10),
+              Container(
+                padding: const EdgeInsets.symmetric(horizontal: 9),
+                height: 50,
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(10),
+                  color: const Color.fromRGBO(83, 232, 139, 0.10),
+                ),
+                child: Row(
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    Padding(
+                      padding: const EdgeInsets.only(right: 17),
+                      child: Container(
+                        height: 33,
+                        width: 33,
+                        decoration: BoxDecoration(
+                          color: Colors.white,
+                          borderRadius: BorderRadius.circular(45),
+                        ),
+                        child: const Icon(Icons.list, color: Colors.green),
+                      ),
+                    ),
+                    const Text(
+                      "Cooking Steps",
+                      style:
+                          TextStyle(fontSize: 14, fontWeight: FontWeight.w600),
+                    ),
+                    const Spacer(),
+                    const Text(
+                      "",
+                      style: TextStyle(fontSize: 14),
+                    ),
+                    IconButton(
+                      onPressed: () {
+                        setState(() {
+                          showHidden = !showHidden;
+                        });
+                      },
+                      icon: showHidden
+                          ? const Icon(Icons.arrow_downward)
+                          : const Icon(Icons.arrow_forward_ios),
+                    ),
+                  ],
+                ),
+              ),
+              const SizedBox(
+                height: 5,
+              ),
+              Visibility(
+                visible: showHidden,
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    for (int i = 0; i < cookingSteps.length; i++)
+                      Padding(
+                        padding: const EdgeInsets.only(bottom: 27, top: 10),
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Row(
+                              children: [
+                                Text(
+                                  "Step ${i + 1}",
+                                  style: const TextStyle(fontSize: 14),
+                                ),
+                                IconButton(
+                                  onPressed: () => selectStepImage(i),
+                                  icon: const Icon(Icons.add_a_photo),
+                                ),
+                              ],
+                            ),
+                            SizedBox(
+                              child: TextFormField(
+                                maxLines: 15,
+                                minLines: 3,
+                                autocorrect: true,
+                                decoration: InputDecoration(
+                                  focusedBorder: OutlineInputBorder(
+                                    borderRadius: BorderRadius.circular(10),
+                                    borderSide: const BorderSide(
+                                      color: Colors.green,
+                                      width: 2.0,
+                                    ),
+                                  ),
+                                  enabledBorder: OutlineInputBorder(
+                                    borderRadius: BorderRadius.circular(10),
+                                    borderSide: const BorderSide(),
+                                  ),
+                                  hintText: "Steps for the meal",
+                                ),
+                                onChanged: (value) {
+                                  cookingSteps[i] = value;
+                                },
+                              ),
+                            ),
+                            if (i == cookingSteps.length - 1)
+                              Align(
+                                alignment: Alignment.centerRight,
+                                child: IconButton(
+                                  onPressed: removeLastCookingStep,
+                                  icon: const Icon(Icons.remove_circle_outline),
+                                ),
+                              ),
+                          ],
+                        ),
+                      ),
+                    GestureDetector(
+                      onTap: addCookingStep,
+                      child: Container(
+                        width: 90,
+                        decoration: BoxDecoration(
+                          border: Border.all(color: Colors.black),
+                        ),
+                        child: const Row(
+                          children: [
+                            Icon(Icons.add),
+                            Text(
+                              "ADD STEP",
+                              style: TextStyle(fontSize: 12),
+                            ),
+                          ],
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
               const SizedBox(
                 height: 40,
               ),
@@ -384,33 +540,30 @@ class _AddRecipeState extends State<AddRecipe> {
                   ),
                 ),
               ),
-              Padding(
-                padding: const EdgeInsets.only(bottom: 27),
-                child: GestureDetector(
-                  onTap: () {},
-                  child: Container(
-                    margin: const EdgeInsets.only(top: 16),
-                    width: 206,
-                    height: 54,
-                    decoration: BoxDecoration(
-                      gradient: const LinearGradient(
-                        begin: Alignment.centerRight,
-                        end: Alignment.centerLeft,
-                        colors: [
-                          Color(0xff15BE77),
-                          Color(0xff53E88B),
-                        ],
-                      ),
-                      borderRadius: BorderRadius.circular(15),
+              GestureDetector(
+                onTap: () {},
+                child: Container(
+                  margin: const EdgeInsets.only(top: 16),
+                  width: 206,
+                  height: 54,
+                  decoration: BoxDecoration(
+                    gradient: const LinearGradient(
+                      begin: Alignment.centerRight,
+                      end: Alignment.centerLeft,
+                      colors: [
+                        Color(0xff15BE77),
+                        Color(0xff53E88B),
+                      ],
                     ),
-                    child: const Center(
-                      child: Text(
-                        "Save Recipe",
-                        style: TextStyle(
-                          fontSize: 16,
-                          fontWeight: FontWeight.w600,
-                          color: Colors.white,
-                        ),
+                    borderRadius: BorderRadius.circular(15),
+                  ),
+                  child: const Center(
+                    child: Text(
+                      "Save Recipe",
+                      style: TextStyle(
+                        fontSize: 16,
+                        fontWeight: FontWeight.w600,
+                        color: Colors.white,
                       ),
                     ),
                   ),
